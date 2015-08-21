60ac82ccc48d43db9147690667c2f8ba9e20b1f4
Jun Wang <wj1918@hotmail.com>
2015-08-21 04:13:23 +0000

    added login requited for attendance sheet

--- wsgi/mccc/children/views.py
+++ wsgi/mccc/children/views.py
@@ -3,7 +3,9 @@ from django.http import HttpResponse
 from children.models import CmMaster 
 from django.shortcuts import render
 import datetime
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def attendancesheet(request,grade,month,num_weeks):
     firstday=datetime.date(datetime.date.today().year,int(month),1)
     sunday=firstday + datetime.timedelta(days=-firstday.weekday()-1, weeks=1)
