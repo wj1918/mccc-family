dc22c6f430b85f68430e8cd466b2ae0a22f30353
Jun Wang <wj1918@hotmail.com>
2015-10-02 23:50:57 +0000

    only superuser can access database

--- wsgi/mccc/mccc/views.py
+++ wsgi/mccc/mccc/views.py
@@ -2,7 +2,7 @@ from social.apps.django_app.views import complete as django_complete
 from django.http import HttpResponse
 import datetime
 from cipher import aes_cipher
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import user_passes_test
 from django.shortcuts import redirect
 from mccc import settings
 
@@ -12,7 +12,7 @@ def mccc_complete(request, backend, *args, **kwargs):
     response = django_complete(request, backend, *args, **kwargs)
     return response
 
-@login_required(login_url='/')
+@user_passes_test(lambda u: u.is_superuser, login_url='/')
 def database(request):
     credential=aes_cipher.encrypt("%s:%s" % (settings.DATABASES['default']['USER'], settings.DATABASES['default']['PASSWORD']) ) 
     return redirect('/phpMyAdmin/signon.php?c='+credential)
