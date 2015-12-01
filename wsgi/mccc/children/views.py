db24114a24b275c194390f5eff519421db2c2c42
Jun Wang <wj1918@hotmail.com>
2015-12-01 04:15:40 +0000

    order by name

--- wsgi/mccc/children/views.py
+++ wsgi/mccc/children/views.py
@@ -17,20 +17,20 @@ def attendancesheet(request,grade,month,num_weeks):
     grades=grade.split("+")
     all_season=['Winter','Winter','Spring','Spring','Spring','Summer','Summer','Summer','Fall','Fall','Fall','Winter']
     quarter=all_season[int(month)-1]
-    children_list = CmMaster.objects.filter(ssgrade__in=grades,ssactive='Active').order_by('ssgrade')
+    children_list = CmMaster.objects.filter(ssgrade__in=grades,ssactive='Active').order_by('ssgrade','first_last')
     context ={'children_list': children_list, "grade":grade, "sundays":sundays, "quarter":quarter, } 
     return render(request, 'children/attendancesheet.html', context)
 
 @login_required
 def parentemail(request,grade):
     grades=grade.split("+")
-    children_list = CmMaster.objects.filter(ssgrade__in=grades,ssactive='Active').exclude(email__isnull=True).exclude(email__exact='').order_by('ssgrade')
+    children_list = CmMaster.objects.filter(ssgrade__in=grades,ssactive='Active').exclude(email__isnull=True).exclude(email__exact='').order_by('ssgrade','first_last')
     context ={'children_list': children_list, "grade":grade, } 
     return render(request, 'children/parentemail.html', context)
 
 @login_required
 def parentcontact(request,grade):
     grades=grade.split("+")
-    children_list = CmMaster.objects.filter(ssgrade__in=grades,ssactive='Active').order_by('ssgrade')
+    children_list = CmMaster.objects.filter(ssgrade__in=grades,ssactive='Active').order_by('ssgrade','first_last')
     context ={'children_list': children_list, "grade":grade, } 
     return render(request, 'children/parentcontact.html', context)
