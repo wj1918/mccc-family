d606b0d1ab5c732ac039f80c1a1c8277b6a824fc
Jun Wang <wj1918@hotmail.com>
2015-12-04 22:46:08 +0000

    add the registry file

---
+++ wsgi/mccc/children/autocomplete_light_registry.py
@@ -0,0 +1,33 @@
+from django.db.models import Q
+import autocomplete_light
+from family.models import Person
+
+class PersonAutocomplete(autocomplete_light.AutocompleteModelBase):
+    autocomplete_js_attributes={'placeholder': 'Other model name ?',}
+    model = Person
+
+    def choices_for_request(self):
+        if self.request.user.is_staff:
+            q = self.request.GET.get('q', '')
+            if q:
+                import re
+                choices = self.choices.all()
+                names=re.findall(r"[\w']+", q)
+                name1=names[0].strip()
+                if len(names) >1:
+                    name2=names[1].strip()
+                    if len(name2)>0:
+                        choices = choices.filter(
+                            (Q(first__icontains=name1) & Q(last__icontains=name2)) |
+                            (Q(first__icontains=name2) & Q(last__icontains=name1))
+                        )
+                    else:    
+                        choices = choices.filter(Q(first__icontains=name1)| Q(last__icontains=name1))
+                else:
+                    choices = choices.filter(Q(first__icontains=name1) | Q(last__icontains=name1))
+
+            return self.order_choices(choices)[0:self.limit_choices]
+        else:
+            return Person.objects.none()
+    
+autocomplete_light.register(PersonAutocomplete)
