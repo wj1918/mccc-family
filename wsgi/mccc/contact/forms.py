f7889af6c1deecb67b4b936bac3d037820708b67
Jun Wang <wj1918@hotmail.com>
2015-12-23 22:51:22 +0000

    save work

--- wsgi/mccc/contact/forms.py
+++ wsgi/mccc/contact/forms.py
@@ -2,7 +2,9 @@
 from django import forms
 
 class ContactForm(forms.Form):
-    subject = forms.CharField(max_length=100)
-    message = forms.CharField(widget=forms.Textarea)
-    sender = forms.EmailField()
-    cc_myself = forms.BooleanField(required=False)
+    home_address=forms.CharField(max_length=200)
+    home_phone=forms.CharField(max_length=100)
+    cell_phone1=forms.CharField(max_length=100)
+    cell_phone2=forms.CharField(max_length=100)
+
+    
