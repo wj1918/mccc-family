69bbee834802dc85554cff48a4d05e072d504cde
Jun Wang <wj1918@hotmail.com>
2016-01-01 15:22:15 +0000

    changed form

--- wsgi/mccc/contact/forms.py
+++ wsgi/mccc/contact/forms.py
@@ -2,7 +2,10 @@
 from django import forms
 
 class ContactForm(forms.Form):
-    home_address=forms.CharField(max_length=200)
+    address=forms.CharField(max_length=200)
+    city=forms.CharField(max_length=100)
+    state=forms.CharField(max_length=100)
+    zip=forms.CharField(max_length=100)
     home_phone=forms.CharField(max_length=100)
     cell_phone1=forms.CharField(max_length=100)
     cell_phone2=forms.CharField(max_length=100)
