3f1ef4047aee66b379bcbe0f94b4b467ba1cb852
Jun Wang <wj1918@hotmail.com>
2016-03-06 22:14:01 +0000

    added download as excel

--- wsgi/mccc/children/admin.py
+++ wsgi/mccc/children/admin.py
@@ -1,3 +1,8 @@
+import unicodecsv
+from functools import wraps
+from collections import OrderedDict
+from django.http import HttpResponse
+from singledispatch import singledispatch  # pip install singledispatch
 from django.contrib import admin
 from django.contrib.admin import AdminSite
 from django.http import HttpResponseRedirect
@@ -7,6 +12,146 @@ from children.models import CmMaster
 from family.models import Person
 import autocomplete_light
 
+def prep_field(obj, field):
+    """
+    (for download_as_csv action)
+    Returns the field as a unicode string. If the field is a callable, it
+    attempts to call it first, without arguments.
+    """
+    if '__' in field:
+        bits = field.split('__')
+        field = bits.pop()
+
+        for bit in bits:
+            obj = getattr(obj, bit, None)
+
+            if obj is None:
+                return ""
+
+    attr = getattr(obj, field)
+    output = attr() if callable(attr) else attr
+    return unicode(output).encode('utf-8') if output is not None else ""
+
+@singledispatch
+def download_as_excel(modeladmin, request, queryset):
+    """
+    Generic exel export admin action.
+
+    Example:
+
+        class ExampleModelAdmin(admin.ModelAdmin):
+            raw_id_fields = ('field1',)
+            list_display = ('field1', 'field2', 'field3',)
+            actions = [download_as_csv,]
+            download_as_csv_fields = [
+                'field1',
+                ('foreign_key1__foreign_key2__name', 'label2'),
+                ('field3', 'label3'),
+            ],
+            download_as_csv_header = True
+    """
+    fields = getattr(modeladmin, 'download_as_excel_fields', None)
+    exclude = getattr(modeladmin, 'download_as_excel_exclude', None)
+    header = getattr(modeladmin, 'download_as_excel_header', True)
+    verbose_names = getattr(modeladmin, 'download_as_excel_verbose_names', True)
+
+    opts = modeladmin.model._meta
+
+    def fname(field):
+        if verbose_names:
+            return unicode(field.verbose_name).capitalize()
+        else:
+            return field.name
+
+    # field_names is a map of {field lookup path: field label}
+    if exclude:
+        field_names = OrderedDict(
+            (f.name, fname(f)) for f in opts.fields if f not in exclude
+        )
+    elif fields:
+        field_names = OrderedDict()
+        for spec in fields:
+            if isinstance(spec, (list, tuple)):
+                field_names[spec[0]] = spec[1]
+            else:
+                try:
+                    f, _, _, _ = opts.get_field_by_name(spec)
+                except FieldDoesNotExist:
+                    field_names[spec] = spec
+                else:
+                    field_names[spec] = fname(f)
+    else:
+        field_names = OrderedDict(
+            (f.name, fname(f)) for f in opts.fields
+        )
+
+    # response = HttpResponse(content_type='text/csv')
+    # response['Content-Disposition'] = 'attachment; filename=%s.csv' % (
+    #         unicode(opts).replace('.', '_')
+    #     )
+
+    import openpyxl
+    from openpyxl.cell import get_column_letter
+
+    response = HttpResponse(content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+    file_name = "CM Master.xlsx";
+    response['Content-Disposition'] = 'attachment; filename='+file_name;
+    wb = openpyxl.Workbook()
+    ws = wb.get_active_sheet()
+    ws.title = "CM Master" 
+
+    row_num = 0
+    col_num = 0;
+
+    # writer = csv.writer(response)
+    # writer = unicodecsv.writer(response, encoding='utf-8')
+    
+    if header:
+        col_num = 0;
+        for headrname in field_names.values():
+            c = ws.cell(row=row_num + 1, column=col_num + 1)
+            c.value = headrname
+            col_num=col_num+1
+    row_num=row_num+1
+    for obj in queryset:
+        # writer.writerow([prep_field(obj, field) for field in field_names.keys()])
+        col_num = 0;
+        for field in field_names.keys():
+            c = ws.cell(row=row_num + 1, column=col_num + 1)
+            c.value = prep_field(obj, field)
+            col_num=col_num+1
+        row_num=row_num+1
+
+    wb.save(response)
+    return response
+
+download_as_excel.short_description = "Download selected objects as Excel file"
+
+@download_as_excel.register(basestring)
+def _(description):
+    """
+    (overridden dispatcher)
+    Factory function for making a action with custom description.
+
+    Example:
+
+        class ExampleModelAdmin(admin.ModelAdmin):
+            raw_id_fields = ('field1',)
+            list_display = ('field1', 'field2', 'field3',)
+            actions = [download_as_csv("Export Special Report"),]
+            download_as_csv_fields = [
+                'field1',
+                ('foreign_key1__foreign_key2__name', 'label2'),
+                ('field3', 'label3'),
+            ],
+            download_as_csv_header = True
+    """
+    @wraps(download_as_excel)
+    def wrapped_action(modeladmin, request, queryset):
+        return download_as_excel(modeladmin, request, queryset)
+    wrapped_action.short_description = description
+    return wrapped_action
+
 class ChildrenSite(AdminSite):
     site_header = 'Children'
     
@@ -14,14 +159,65 @@ class ChildrenSite(AdminSite):
         return redirect('%s?next=%s' % (reverse('home'), request.REQUEST.get('next', '')))
 
 class CmMasterAdmin(admin.ModelAdmin):
-    list_display = ['first_last','first_last','ssgrade','ssactive','fname','lname','chinese_name','gender','grade','dob',
+    list_display = ['first_last','ssgrade','ssactive','fname','lname','chinese_name','gender','grade','dob',
     'allergies_medical_conditions_medications','fathers_english_name','fathers_chinese_name_if_available','mothers_english_name','mother_chinese_name_if_available',
     'email','street','city','state','zip','home','fathers_office','fathers_cell','mothers_office','mothers_cell','alternate_contact_name','alt_contact_main_phone',
-    'altcont','mccc','group','assign','christianfather','christianmother','remarks','felly']
+    'altcont','mccc','group','assign','christianfather','christianmother','remarks','felly','vbs_2010','vbs_2011','vbs_2012','vbs_2013','vbs_2014','vbs_2015',]
     search_fields = ['first_last','fname','lname','chinese_name','allergies_medical_conditions_medications','fathers_english_name','fathers_chinese_name_if_available','mothers_english_name','mother_chinese_name_if_available',
     'email','street','city','state','zip','home']
     list_filter = ['ssactive','ssgrade','vbs_2015','vbs_2014','vbs_2013','vbs_2012','vbs_2011','vbs_2010']
     form = autocomplete_light.modelform_factory(CmMaster,exclude=[])
+    actions = [download_as_excel("Download selected objects as Excel file"),]
+    download_as_excel_fields=['first_last',
+       'ssgrade',
+       'ssactive',
+       'fname',
+       'lname',
+       'chinese_name',
+       'gender',
+       'grade',
+       'dob',
+       'allergies_medical_conditions_medications',
+       'fathers_english_name',
+       'fathers_chinese_name_if_available',
+       'mothers_english_name',
+       'mother_chinese_name_if_available',
+       'email',
+       'street',
+       'city',
+       'state',
+       'zip',
+       'home',
+       'fathers_office',
+       'fathers_cell',
+       'mothers_office',
+       'mothers_cell',
+       'alternate_contact_name',
+       'alt_contact_main_phone',
+       'altcont',
+       'mccc',
+       'group',
+       'assign',
+       'christianfather',
+       'christianmother',
+       'remarks',
+       'felly',         
+       'vbs_2010',
+       'vbs_2011',
+       'vbs_2012',
+       'vbs_2013',
+       'vbs_2014',
+       'vbs_2015',
+    ]
+    download_as_excel_header = True
+
+    def get_actions(self, request):
+        actions = super(CmMasterAdmin, self).get_actions(request)
+        if request.user.is_superuser:
+            return actions
+        else:
+            return None
+
     
 # for the person raw_id picker widget 
 # The raw_id_fields widget shows a magnifying glass button next to the field which allows users to search for and select a value
