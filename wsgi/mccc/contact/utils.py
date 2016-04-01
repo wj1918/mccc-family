aec5d45c86636d30ec0a2114047757a0b8fb0547
Jun Wang <wj1918@hotmail.com>
2016-04-01 02:35:23 +0000

    remove logging tree import

--- wsgi/mccc/contact/utils.py
+++ wsgi/mccc/contact/utils.py
@@ -2,7 +2,6 @@ import logging
 import random
 import string
 import datetime
-from logging_tree import printout
 from django.conf import settings
 from django.shortcuts import get_object_or_404
 from family.models import Family
